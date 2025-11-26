82dee690e5ba848fd3650f489485a7ee866519be
michael <michael.lukyanov@student.kdg.be>
2025-11-26 21:41:10 +0100

    added llm game to react frontend

--- frontend/src/router/AppRoutes.tsx
+++ frontend/src/router/AppRoutes.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { GamesPage } from "../pages/GamePage";
-import { ConnectFourPage } from "../pages/ConnectFourPage";
 import GameAchievementsPage from "../pages/GameAchievementsPage.tsx";
+import {FullScreenFrame} from "../components/connect4/FullScreenFrame.tsx";
 
 export function AppRoutes() {
     return (
@@ -9,7 +9,9 @@ export function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Navigate to="/games" replace />} />
                 <Route path="/games" element={<GamesPage />} />
-                <Route path="/games/connect4" element={<ConnectFourPage />} />
+
+                <Route path="/games/connect4" element={<FullScreenFrame src="/connect4/index.html" />} />
+
                 <Route path="/games/:gameId/achievements" element={<GameAchievementsPage />} />
                 <Route path="*" element={<p>Not found</p>} />
             </Routes>
