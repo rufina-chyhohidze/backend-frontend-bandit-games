ec2a2367e3e5db3d97a6a025728a0acc0ea11f04
Philipe Souza <62403846+PhChevico@users.noreply.github.com>
2025-11-28 12:07:23 +0100

    removing annotation to fix modulith display on packages

--- backend/src/main/java/be/kdg/banditgames/BackendApplication.java
+++ backend/src/main/java/be/kdg/banditgames/BackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.modulith.Modulith;
 
 @Modulith
-@SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
