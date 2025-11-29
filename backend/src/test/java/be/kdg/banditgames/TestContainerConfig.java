fee407357e2ad1cb7f6524719c4474de2aeaa960
Philipe Souza <62403846+PhChevico@users.noreply.github.com>
2025-11-29 20:49:10 +0100

    cicd

--- backend/src/test/java/be/kdg/banditgames/TestContainerConfig.java
+++ backend/src/test/java/be/kdg/banditgames/TestContainerConfig.java
@@ -3,6 +3,7 @@ package be.kdg.banditgames;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -14,6 +15,11 @@ public class TestContainerConfig {
     PostgreSQLContainer<?> postgreSQLContainer() {
         return new PostgreSQLContainer<>(DockerImageName.parse("postgres:18.1"));
     }
-    
+
+    @Bean
+    @ServiceConnection
+    MongoDBContainer mongoDBContainer() {
+        return new MongoDBContainer(DockerImageName.parse("mongo:6.0"));
+    }
     
 }
