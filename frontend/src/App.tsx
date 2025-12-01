5375c1dfec5008dabd39b051278a8065109f805b
rufinachyhohidze <rufina.chyhohidze@student.kdg.be>
2025-12-01 15:41:18 +0100

    fix: add chatbot - front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SecurityContextProvider from "./context/SecurityContextProvider";
 import { AppRoutes } from "./router/AppRoutes";
+import {ChatProvider} from "./context/ChatProvider.tsx";
+import ChatbotWidget from "./components/Chatbot/ChatbotWidget";
 
 const queryClient = new QueryClient();
 
@@ -10,9 +12,12 @@ function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <SecurityContextProvider>
+                <ChatProvider>
                 <BrowserRouter>
                     <AppRoutes/>
+                    <ChatbotWidget />
                 </BrowserRouter>
+                </ChatProvider>
             </SecurityContextProvider>
         </QueryClientProvider>
     );
