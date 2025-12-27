8313893242bb93246c11de2cc0d11e8c21fa332b
Anna Mestres <anna.mestrescasadesus@student.kdg.be>
2025-12-27 18:58:31 +0100

    fix: fix event publication table

---
+++ backend/src/main/java/be/kdg/banditgames/config/FlywayConfig.java
@@ -0,0 +1,23 @@
+package be.kdg.banditgames.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean(initMethod = "migrate")
+    @DependsOn("entityManagerFactory") // This ensures JPA runs first
+    public Flyway flyway(DataSource dataSource) {
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .baselineVersion("0")
+                .load();
+    }
+}
